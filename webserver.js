1a51f03a558381ed40cf1a777a504d6af339760a
Tim <tim.hardyman@thehivegroup.net>
2014-02-12 13:53:57 +0000

    Hook up webserver to drone

--- webserver.js
+++ webserver.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server),
+  flappyDrone = require('server'),
+  client = flappyDrone.clientEmitter,
+  droneStatus = null;
 
 server.listen(8080);
+
+client.on('navdata', function(data){
+    if(!data.demo) return;
+    // If height below tolerance, end the game!
+    droneStatus = data;
+  });
 
 app.use(express.static('public'));
 
 io.sockets.on('connection', function (socket) {
 
-  socket.emit('drone-status', { hello: 'world' });
+  socket.emit('drone-status', droneStatus);
 
   socket.on('tap', function (data) {
     console.log('Tap detected');
+    flappyDrone.jump();
   });
 
 });
