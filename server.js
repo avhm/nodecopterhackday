7a68ea2c8494515364483ec2653cfab2cc2624c2
Andy Trevorah <andy@troupe.co>
2014-02-12 11:46:07 +0000

    moved input stuff into server

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
+var emitter = new EventEmitter();
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (text) {
+  emitter.emit('enter');
+  if (text === 'quit\n') {
+    done();
+  }
+});
+
+function done() {
+  console.log('Now that process.stdin is paused, there is nothing more to do.');
+  this.stop();
+  this.land();
+  process.exit();
+}
+
 client.takeoff();
 
-client
-  .after(1000, function() {
-    // this.clockwise(1);
-  })
-  .after(3000, function() {
-    
-    // this.animate('flipLeft', 15);
-  })
-  .after(10000, function() {
-    this.stop();
-    this.land();
-  });
+var step;
+
+emitter.on('enter', function() {
+
+  if(step){
+    clearTimeout(step);
+  }
+  
+  client.up(1);
+
+  step = setTimeout(function(){
+    client.down(.7);
+  }, 400);
+
+});
+
+emitter.on('enter', function() {
+
+});
+
+module.exports = emitter;
