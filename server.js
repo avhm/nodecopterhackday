b3555716dadb4a154a9d614e03bc8588239c407e
Anthony Mann <anthony@make-us-proud.com>
2014-02-12 10:55:11 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+
+client.takeoff();
+
+client
+  .after(1000, function() {
+    this.clockwise(1);
+  })
+  .after(3000, function() {
+    this.stop();
+    // this.animate('flipLeft', 15);
+  })
+  .after(10000, function() {
+    
+    this.land();
+  });
