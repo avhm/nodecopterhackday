54c6dc9527bacc0baa4ea38215dc8f9550709b23
Anthony Mann <anthony@make-us-proud.com>
2014-02-12 12:05:32 +0000

    adding some basic control

--- server.js
+++ server.js
@@ -3,25 +3,46 @@ var util = require('util');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-input.on('quit', function() {
-  client.stop();
-  client.land();
-  process.exit();
-});
+var minDroneHeight = .3;
+var gameon = false;
+
+input.on('quit', quit);
 
 client.takeoff();
 
-var step;
+function quit(){
+  client.stop();
+  client.land();
+  console.log('Killing!')
+  setTimeout(process.exit, 200);
+}
 
+function start(){
+  client.on('navdata', function(data){
+    if(!data.demo) return;
+    // If height below tolerance, end the game!
+    if(data.demo.altitude < minDroneHeight){
+      console.log('GAME OVER!!!');
+      quit();
+    }
+  });
+}
+
+var currentJump;
 input.on('enter', function() {
 
-  if(step){
-    clearTimeout(step);
+  if(!gameon){
+    start();
+    gameon = true;
+  }
+
+  if(currentJump){
+    clearTimeout(currentJump);
   }
   
   client.up(1);
 
-  step = setTimeout(function(){
+  currentJump = setTimeout(function(){
     client.down(.7);
   }, 400);
 
