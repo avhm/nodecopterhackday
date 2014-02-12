a0b51e00b33977d0260c4a20013b795b4e3b0e42
Andy Trevorah <andy@troupe.co>
2014-02-12 11:28:42 +0000

    input emits 'enter' event

--- input.js
+++ input.js
@@ -1,16 +1,25 @@
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-  process.stdin.on('data', function (text) {
-    console.log('up');
-    if (text === 'quit\n') {
-      done();
-    }
-  });
+var emitter = new EventEmitter();
 
-  function done() {
-    console.log('Now that process.stdin is paused, there is nothing more to do.');
-    process.exit();
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (text) {
+  emitter.emit('enter');
+  if (text === 'quit\n') {
+    done();
   }
-  
+});
+
+function done() {
+  console.log('Now that process.stdin is paused, there is nothing more to do.');
+  process.exit();
+}
+
+emitter.on('enter', function() {
+  console.log('up!');
+});
+
+module.exports = emitter;
