f11b16028c5efeb3c689443cba1d6fa18830284e
Andy Trevorah <andy@troupe.co>
2014-02-12 11:50:35 +0000

    input module just emits events

--- input.js
+++ input.js
@@ -1,7 +1,5 @@
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
-var arDrone = require('ar-drone');
-var client = arDrone.createClient();
 
 var emitter = new EventEmitter();
 
@@ -9,39 +7,11 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function (text) {
-  emitter.emit('enter');
   if (text === 'quit\n') {
-    done();
+    emitter.emit('quit');
+  } else {
+    emitter.emit('enter');
   }
-});
-
-function done() {
-  console.log('Now that process.stdin is paused, there is nothing more to do.');
-  this.stop();
-  this.land();
-  process.exit();
-}
-
-client.takeoff();
-
-var step;
-
-emitter.on('enter', function() {
-
-  if(step){
-    clearTimeout(step);
-  }
-  
-  client.up(1);
-
-  step = setTimeout(function(){
-    client.down(.7);
-  }, 400);
-
-});
-
-emitter.on('enter', function() {
-
 });
 
 module.exports = emitter;
