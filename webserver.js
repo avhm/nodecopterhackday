ea38f7ac9ec83241c48d826c67d5ba6bec831bec
Tim <tim.hardyman@thehivegroup.net>
2014-02-12 13:20:10 +0000

    Socket io

--- webserver.js
+++ webserver.js
@@ -1,6 +1,25 @@
-var socket = require('socket.io-client')('http://localhost');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-socket.on('connect', function(){
-	socket.on('event', function(data){});
-	socket.on('disconnect', function(){});
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
